ab835a56b34304f77508902243d87f84553cde9a
Meisya Maria Alfanita Maria <99252880+Meisya7@users.noreply.github.com>
2022-10-31 05:02:31 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<html>
+  <head>
+    <title>Belajar PHP</title>
+  </head>
+  <body>
+
+    <h1>Loop PHP</h1>
+    
+    <?php 
+      //----------- Pengulangan ------
+      //for(variabelawal(mulai), batas(syarat), perubahan)
+
+      $hewan = ['anjing', 'babi', 'cicak', 'domba', 'buaya'];
+
+      // for($i=0; $i<count($hewan); $i++)
+      // {
+      // echo "-----------------";
+      // echo $hewan[$i];
+      // echo "----------------- <br>";
+      // }
+ 
+      // foreach($hewan as $h){
+      // echo "-----------------";
+      // echo $h;
+      // echo "----------------- <br>";
+      // }
+
+      // $data = ['nama' => 'hilman',
+      //       'umur'=>24, 
+      //       'sifat'=> 'malas'];
+      //
+      // foreach($data as $key => $value){
+      //  echo $value . "<br>";
+      // }
+
+      //----------- 3) while & do while ----------
+      //while(syarat)
+
+      $i = 5;
+
+      while( $i < count($hewan) ){
+        echo $hewan[$i]. "<br>";
+        $i++;
+      }
+
+      do{
+        echo '------';
+        echo $hewan[$i]. "<br>";
+        $i++;  
+        }while( $i < count($hewan));
+    ?> 
+ 
+  </body>
+</html>
